70d833915a73e3d71436a9c68f230bbe649c510c
jjjjustine <catingan.janejustine@gmail.com>
2024-09-22 14:51:47 +0800

    Added Simple Implementation using Array

---
+++ src/MultidimensionalArray/MultidimensionalArray.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MultidimensionalArray;
+
+/**
+ *
+ * @author Jane Justine
+ */
+public class MultidimensionalArray {
+     public static void main(String[] args) {
+    
+     }
+}
