717dadf20a9adde6c1a5ab59fcdf99b945ef0663
jjjjustine <catingan.janejustine@gmail.com>
2024-09-24 15:10:12 +0800

    Adeded some code

---
+++ src/arraylist/ItemManager.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arraylist;
+/**
+ *
+ * @author Jane Justine
+ */
+import java.util.ArrayList;
+public class ItemManager {
+    
+    private ArrayList<String> items;
+    public ItemManager(){
+    items= new ArrayList<>();
+ 
+    }
+
+}
