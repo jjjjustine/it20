4e54d0e9f5fa4c055f2d658c81837416c6b7235f
jjjjustine <catingan.janejustine@gmail.com>
2024-09-24 15:24:57 +0800

    Adeded Display Item and Add Item

--- src/arraylist/ItemManager.java
+++ src/arraylist/ItemManager.java
@@ -14,7 +14,16 @@ public class ItemManager {
     private ArrayList<String> items;
     public ItemManager(){
     items= new ArrayList<>();
- 
+    }
+    
+    //this will display items on the array list
+      public void displayItems() {
+        System.out.println("Items in the list: " + items);
+    }
+      //this will add items on the array list
+      public void addItems(String item) {
+        items.add(item);
+        System.out.println("You Inserted: " + items);
+    }
     }
 
-}
--- src/arraylist/Main.java
+++ src/arraylist/Main.java
@@ -13,6 +13,18 @@ import java.util.Scanner;
 public class Main {
    public static void main(String[] args) {
        
+       Scanner sc = new Scanner(System.in);
  ItemManager list = new ItemManager();  
+  String item, newItem, finditem;
+        int index;
+        //gi tawag niya ang display items sa sub class nga ItemManager
+        list.displayItems();
+    
+        System.out.println("Enter an item you want to add: ");
+        item = sc.nextLine();
+        
+        //mabutang diri ang gi add nga name
+        list.addItems(item);
+          System.out.println(".........................");
 }
 }
