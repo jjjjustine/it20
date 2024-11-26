93762290d2915ca5a797dbeba9e4cb3484f72b02
jjjjustine <catingan.janejustine@gmail.com>
2024-11-26 19:50:38 +0800

    Implemented Selection Sort algorithm with logic to find the minimum element and swap in each iteration

---
+++ src/topic_6_soting/SelectionSort.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package topic_6_soting;
+import java.util.Scanner;
+/**
+ *
+ * @author Jane Justine
+ */
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap the found minimum element with the first element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+}
