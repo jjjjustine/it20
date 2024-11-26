69523dfa4a2b2ec8825d82cd27ec2a76e0a1cdae
jjjjustine <catingan.janejustine@gmail.com>
2024-11-26 19:27:52 +0800

    Added MergeSort method to sort an array using the merge sort algorithm

---
+++ src/topic_6_soting/MergeSort.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package topic_6_soting;
+import java.util.Scanner;
+/**
+ *
+ * @author Jane Justine
+ */
+public class MergeSort {
+     // MergeSort method
+    public static void mergeSort(int[] arr) {
+        if (arr.length < 2) {
+            return;
+        }
+
+        int mid = arr.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[arr.length - mid];
+
+        System.arraycopy(arr, 0, left, 0, mid);
+        System.arraycopy(arr, mid, right, 0, arr.length - mid);
+
+        mergeSort(left);
+        mergeSort(right);
+
+        merge(arr, left, right);
+    }
+
+}
